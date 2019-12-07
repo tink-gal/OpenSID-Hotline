71f2f34b43bc1b284435f854525a1d0f3608488e
tink-gal <58603107+tink-gal@users.noreply.github.com>
2019-12-07 14:56:04 +0700

    Add files via upload

--- donjo-app/views/hotline/table.php
+++ donjo-app/views/hotline/table.php
@@ -13,7 +13,7 @@
 					<div class="box box-info">
             <div class="box-header with-border">
 							<a href="<?=site_url("hotline/form")?>" class="btn btn-social btn-flat btn-success btn-sm btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block"  title="Tambah Artikel">
-								<i class="fa fa-plus"></i> Tambah Teks
+								<i class="fa fa-plus"></i> Tambah Nomer
             	</a>
 							<?php if ($this->CI->cek_hak_akses('h')): ?>
 								<a href="#confirm-delete" title="Hapus Data" onclick="deleteAllBox('mainform', '<?=site_url("hotline/delete_all")?>')" class="btn btn-social btn-flat btn-danger btn-sm visible-xs-block visible-sm-inline-block visible-md-inline-block visible-lg-inline-block hapus-terpilih"><i class='fa fa-trash-o'></i> Hapus Data Terpilih</a>
